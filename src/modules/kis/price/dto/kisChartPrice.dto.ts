f1bbd5025a0075fc362659e6c55162f9331a5467
jhleejh98-boop <jhleejh98@gmail.com>
2026-06-18 22:19:23 +0900

    feat: 종목 캔들 조회 API를 1분봉 전용으로 변경

--- src/modules/kis/price/dto/kisChartPrice.dto.ts
+++ src/modules/kis/price/dto/kisChartPrice.dto.ts
@@ -1,3 +1,34 @@
+export interface KisDomesticMinuteChartItem {
+  stck_bsop_date: string; // 영업일자
+  stck_cntg_hour: string; // 체결시각
+  stck_oprc: string; // 시가
+  stck_hgpr: string; // 고가
+  stck_lwpr: string; // 저가
+  stck_prpr: string; // 현재가(종가)
+  cntg_vol?: string; // 거래량
+}
+
+export interface KisDomesticMinuteChartResponse {
+  rt_cd: string;
+  msg1: string;
+  output2?: KisDomesticMinuteChartItem[];
+}
+export interface KisOverseasMinuteChartItem {
+  xymd: string; // 날짜
+  xhms: string; // 시간
+  open: string;
+  high: string;
+  low: string;
+  last: string;
+  evol?: string;
+}
+
+export interface KisOverseasMinuteChartResponse {
+  rt_cd: string;
+  msg1: string;
+  output2?: KisOverseasMinuteChartItem[];
+}
+
 export interface CandleDto {
   time: number;
   open: number;
--- src/modules/stocks/swagger/stockCandles.swagger.ts
+++ src/modules/stocks/swagger/stockCandles.swagger.ts
@@ -1,26 +1,18 @@
 import { ApiQueryOptions, ApiResponseOptions } from "@nestjs/swagger";
 
-export const STOCK_CANDLES_API_DESCRIPTION = "KIS 과거 시세 API를 호출해 FE 차트용 캔들 데이터를 조회합니다.";
-
-export const STOCK_CANDLES_INTERVAL_QUERY: ApiQueryOptions = {
-  name: "interval",
-  required: false,
-  enum: ["1d", "1wk", "1mo"],
-  example: "1d",
-  description: "캔들 간격 (1d: 일봉, 1wk: 주봉, 1mo: 월봉)",
-};
+export const STOCK_CANDLES_API_DESCRIPTION = "KIS 1분봉 시세 데이터를 조회해 FE 차트용 캔들 데이터를 제공합니다.";
 
 export const STOCK_CANDLES_LIMIT_QUERY: ApiQueryOptions = {
   name: "limit",
   required: false,
   type: Number,
   example: 250,
-  description: "조회할 캔들 개수",
+  description: "조회할 1분봉 캔들 개수",
 };
 
 export const STOCK_CANDLES_SUCCESS_API_RESPONSE: ApiResponseOptions = {
   status: 200,
-  description: "종목 캔들 차트 조회 성공",
+  description: "종목 1분봉 캔들 차트 조회 성공",
   schema: {
     example: {
       candles: [
